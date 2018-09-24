f1c20cd9c00017b5d5bf3870f13bd96884c6ac06
IAmAPersson <iamaperson620@gmail.com>
2018-09-24 12:05:37 -0500

    Add files via upload

---
+++ World/World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class World
+{
+    private static Tuple<TimeSpan, DateTime> Value;
+    public static Stopwatch Stop = new Stopwatch();
+    public static void RefreshWorld()
+    {
+        Value = new Tuple<TimeSpan, DateTime>(Stop.Elapsed, DateTime.Now);
+    }
+}
